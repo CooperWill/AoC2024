76b3691e85ed40c9c56820f2064bf929417e539f
WillC <cooperw@sonoma.edu>
2024-12-02 22:06:08 -0800

    Day 3 Part 1 complete

---
+++ day3/day3-part1.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+let input;
+
+
+function readFile(filePath) {
+    try {
+        const data = fs.readFileSync(filePath);
+        input = data.toString()
+    } catch (error) {
+        console.error('Error!@#:', error);
+    }
+}
+
+readFile('./day3input.txt');
+
+//console.log(input)
+
+const bigList = getData(input);
+const justTheStats = trimFat(bigList);
+
+
+
+const cleanArray = separateProblems(justTheStats);
+console.log(doCalcs(cleanArray))
+
+
+function getData(numbers){
+    const numberArray = numbers.trim()
+
+    return numberArray;
+}
+
+function trimFat(string){
+    return string.match(/mul\(\d+,\d+\)/g)?.join(" ") || "";
+
+}
+
+function separateProblems(bigString){
+    const cleanArray = bigString
+        .split(" ");
+
+    return cleanArray;
+}
+
+function doCalcs(array){
+    let total = 0;
+
+    let numbers = array[0].match(/\d+/g).map(Number);
+
+    for(let i = 0; i < array.length; i++){
+        numbers = array[i].match(/\d+/g).map(Number);
+        total += (numbers[0] * numbers[1]);
+    }
+
+    return total;
+
+}
